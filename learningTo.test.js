b80e4a818bb48e8f6142f7577f1b2a2c5d8f7eec
whtlnv <catala.francisco@gmail.com>
2022-07-11 20:36:00 -0500

    testing chapter end :c

--- learningTo.test.js
+++ learningTo.test.js
@@ -2,13 +2,76 @@ const { Province } = require('./Province');
 const { sampleProvinceData } = require('./sampleProvinceData');
 
 describe('province', function () {
+  let asia;
+
+  beforeEach(function () {
+    asia = new Province(sampleProvinceData());
+  });
+
   it('shortfall', function () {
-    console.log(sampleProvinceData);
-    const asia = new Province(sampleProvinceData());
     expect(asia.shortfall).toBe(5);
   });
+
   it('profit', function () {
-    const asia = new Province(sampleProvinceData());
     expect(asia.profit).toBe(230);
+  });
+
+  it('change production', function () {
+    asia.producers[0].production = 20;
+    expect(asia.shortfall).toBe(-6);
+    expect(asia.profit).toBe(292);
+  });
+
+  it('zero demand', function () {
+    asia.demand = 0;
+    expect(asia.shortfall).toBe(-25);
+    expect(asia.profit).toBe(0);
+  });
+
+  it('negative demand', function () {
+    asia.demand = -1;
+    expect(asia.shortfall).toBe(-26);
+    expect(asia.profit).toBe(-10);
+  });
+
+  it('empty string demand', function () {
+    asia.demand = '';
+    expect(asia.shortfall).toBe.NaN;
+    expect(asia.profit).toBe.NaN;
+  });
+});
+
+describe('no producers', function () {
+  let noProducers;
+  beforeEach(function () {
+    const data = {
+      name: 'No proudcers',
+      producers: [],
+      demand: 30,
+      price: 20,
+    };
+
+    noProducers = new Province(data);
+  });
+
+  it('shortfall', function () {
+    expect(noProducers.shortfall).toBe(30);
+  });
+
+  it('profit', function () {
+    expect(noProducers.profit).toBe(0);
+  });
+});
+
+describe('string for producers', function () {
+  it('', function () {
+    const data = {
+      name: 'String producers',
+      producers: '',
+      demand: 30,
+      price: 20,
+    };
+    const prov = new Province(data);
+    expect(prov.shortfall).toBe(0);
   });
 });
