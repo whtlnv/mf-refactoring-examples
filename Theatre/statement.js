dd6ae451493dc13c4e4e0c1a6daad232fc3d5f05
whtlnv <catala.francisco@gmail.com>
2022-07-13 18:24:32 -0500

    Slide statement

--- Theatre/statement.js
+++ Theatre/statement.js
@@ -1,6 +1,5 @@
 function statement(invoice, plays) {
   let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
 
   for (let perf of invoice.performances) {
@@ -8,6 +7,7 @@ function statement(invoice, plays) {
     totalAmount += amountFor(perf);
   }
 
+  let volumeCredits = 0;
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
   }
