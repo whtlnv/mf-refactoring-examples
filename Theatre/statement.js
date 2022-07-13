d68b5b8f59acefb2bb791db8a6e4c538812974ab
whtlnv <catala.francisco@gmail.com>
2022-07-13 18:34:26 -0500

    rename variable

--- Theatre/statement.js
+++ Theatre/statement.js
@@ -51,20 +51,20 @@ function statement(invoice, plays) {
   }
 
   function totalVolumeCredits() {
-    let volumeCredits = 0;
+    let result = 0;
     for (let perf of invoice.performances) {
-      volumeCredits += volumeCreditsFor(perf);
+      result += volumeCreditsFor(perf);
     }
-    return volumeCredits;
+    return result;
   }
 
   function totalAmount() {
-    let totalAmount = 0;
+    let result = 0;
     for (let perf of invoice.performances) {
-      totalAmount += amountFor(perf);
+      result += amountFor(perf);
     }
 
-    return totalAmount;
+    return result;
   }
 }
 
