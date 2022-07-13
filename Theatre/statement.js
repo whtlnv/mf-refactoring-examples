f7837c22cea487846040731f1af8ffc8d5a9bf81
whtlnv <catala.francisco@gmail.com>
2022-07-13 18:33:35 -0500

    change function declaration

--- Theatre/statement.js
+++ Theatre/statement.js
@@ -1,12 +1,10 @@
 function statement(invoice, plays) {
-  let totalAmount = appleSauce();
-
   let result = `Statement for ${invoice.customer}\n`;
   for (let perf of invoice.performances) {
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
   }
 
-  result += `Amount owed is ${usd(totalAmount)}\n`;
+  result += `Amount owed is ${usd(totalAmount())}\n`;
   result += `You earned ${totalVolumeCredits()} credits\n`;
   return result;
 
@@ -60,7 +58,7 @@ function statement(invoice, plays) {
     return volumeCredits;
   }
 
-  function appleSauce() {
+  function totalAmount() {
     let totalAmount = 0;
     for (let perf of invoice.performances) {
       totalAmount += amountFor(perf);
